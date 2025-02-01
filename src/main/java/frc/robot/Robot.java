c3fd24ced0f874accfd45b50c62f40a9b0c2fef6
Micah-Lander <lander.22@daytonstemschool.org>
2025-02-01 11:25:32 -0500

    added robot file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -64,6 +65,10 @@ public class Robot extends TimedRobot {
   @Override
   public void testInit() {
     CommandScheduler.getInstance().cancelAll();
+    CameraServer.startAutomaticCapture();
+    System.out.println("test");
+    // CameraServer.getVideo();
+
   }
 
   @Override
